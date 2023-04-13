c4b5d4c3353cd8feb07f3479025a55de516ef9e6
manidumaru <manidumaru36@gmail.com>
2023-04-13 07:33:17 +0545

    algo lab 2

--- Algorithm/Lab2/algorithm.py
+++ Algorithm/Lab2/algorithm.py
@@ -1,25 +1,57 @@
+import math
 
-def selection(data):
-    for i in range(0, len(data)-1):
+def selection(arr):
+    for i in range(0, len(arr)-1):
         smallest = i+1
-        for j in range(i,len(data)-1):
-            if(data[j] < data[smallest]):
+        for j in range(i,len(arr)-1):
+            if(arr[j] < arr[smallest]):
                 smallest = j
         # print(smallest)
-        print(data[smallest])
-        data[i],data[smallest] = data[smallest],data[i]
+        print(arr[smallest])
+        arr[i],arr[smallest] = arr[smallest],arr[i]
 
-    return data
+    return arr
 
-
-
-def insertion(data):
-    for i in range(0,len(data)-1):
+def insertion(arr):
+    for i in range(0,len(arr)-1):
         j = i
-        while (j>0 and data[j]<data[j-1]):
-            data[j],data[j-1] = data[j-1],data[j]
+        while (j>0 and arr[j]<arr[j-1]):
+            arr[j],arr[j-1] = arr[j-1],arr[j]
             j = j-1
-    return data
+    return arr
 
-data = [2,6,1,0,3,8,4,9]
-print(insertion(data))
+def mergeSort(arr):
+    if(len(arr)>1):
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+        mergeSort(L)
+        mergeSort(R)
+        
+        i=j=k=0
+
+        while(i<len(L) and j<len(R)):
+            if(arr[i] < arr[j]):
+                arr[k] = arr[i]
+                i+=1
+            else:
+                arr[k] = arr[j]
+                j+=1
+            k+=1
+        
+        while(i<len(L)):
+            arr[k] = L[i]
+            i+=1
+            k+=1
+        while(j<len(R)):
+            arr[k] = L[j]
+            j+=1
+            k+=1
+
+
+
+
+arr = [2,6,1,0,3,8,4,9]
+mergeSort(arr)
+print(arr)
+
