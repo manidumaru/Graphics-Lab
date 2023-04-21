695138b32c772e7ded8f7b033dd81f7a1d4490fd
manidumaru <manidumaru36@gmail.com>
2023-04-21 13:19:38 +0545

    bla and dda done

--- Graphics/lab2/BLA.py
+++ Graphics/lab2/BLA.py
@@ -4,43 +4,62 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-def DDA():
-    x1 = 150
-    y1 = 150
+def BLA():
+    x1 = 300
+    y1 = 370
     x2 = 300
-    y2 = 300
+    y2 = 100
     glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(1.0,1.0,0.0)
+    glColor3f(0.0,1.0,1.0)
     glPointSize(3)
-
     slope = (y2-y1)/(x2-x1)
     print(slope)
+    c = y1 - (slope*x1)
 
     if (slope < 1):
         while(x1 != x2):
             glBegin(GL_POINTS)
             glVertex2f(x1,y1)
             glEnd()
-            if (x1<x2):
+            if (x1 < x2):
                 x1 += 1
-                y1 += slope
             else:
                 x1 -= 1
-                y1 -= slope
+            actual_y = (slope*x1) + c
+            if (x1 > x2):
+                d1 = y1 - actual_y
+                d2 = actual_y - (y1-1)
+                if(d1-d2 > 0):
+                    y1 -= 1
+            else:
+                d1 = (y1+1) - actual_y
+                d2 = actual_y - y1
+                if((d1-d2) < 0):
+                    y1 += 1
             glFlush()
     else:
         while(y1 != y2):
             glBegin(GL_POINTS)
             glVertex2f(x1,y1)
             glEnd()
-            if (x1<x2):
-                y1 += 1
-                x1 -= 1/slope
-            else:
+            if(y1 > y2):
                 y1 -= 1
-                x1 -= 1/slope
+            else:
+                y1 += 1
+            actual_x = (y1 - c)/slope
+            if (y1 > y2):
+                d1 = x1 - actual_x
+                d2 = actual_x - (x1-1)
+                if((d1-d2 > 0)):
+                   x1 -= 1
+            else:
+                d1 = (x1+1) - actual_x
+                d2 = actual_x - x1
+                if((d1-d2) < 0):
+                    x1 += 1
             glFlush()
     glutSwapBuffers()
+    return
 
 def main():
     glutInit()
@@ -52,9 +71,7 @@ def main():
     size = numpy.ndarray.tolist(glGetIntegerv(GL_VIEWPORT))
     size = size[2],size[3]
     print(f"Your Window Resolution is: {size}")
-    glutDisplayFunc(DDA)
+    glutDisplayFunc(BLA)
     glutMainLoop()
 
 main()
-
-
